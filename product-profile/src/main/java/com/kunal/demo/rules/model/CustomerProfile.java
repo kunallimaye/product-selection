e8d1bbb52fe03d9d21e064b48dc2ecdaad7c883e
bpmsAdmin <>
2015-03-12 08:53:52 +1100

    Adding labels to the data object {/product-profile/src/main/java/com/kunal/demo/rules/model/CustomerProfile.java}

--- product-profile/src/main/java/com/kunal/demo/rules/model/CustomerProfile.java
+++ product-profile/src/main/java/com/kunal/demo/rules/model/CustomerProfile.java
@@ -7,85 +7,110 @@ package com.kunal.demo.rules.model;
  * @author kunallimaye
  *
  */
-public class CustomerProfile {
-	private Customer customer;
-	private int age;
-	private AffordabilityConcernType affordabilityConcern;
-	private long coverAmount;
-	private EmploymentDurationType employmentDuration;
+public class CustomerProfile
+{
+   @org.kie.api.definition.type.Label("Customer instance")
+   private Customer customer;
+   @org.kie.api.definition.type.Label("Age")
+   private int age;
+   private AffordabilityConcernType affordabilityConcern;
+   @org.kie.api.definition.type.Label("Insurance cover amount")
+   private long coverAmount;
+   private EmploymentDurationType employmentDuration;
 
-	public CustomerProfile(){
-		
-	}
-	
-	/**
-	 * @param customer
-	 * @param age
-	 * @param affordabilityConcern
-	 * @param coverAmount
-	 * @param employmentDuration
-	 */
-	public CustomerProfile(Customer customer, int age,
-			AffordabilityConcernType affordabilityConcern, long coverAmount,
-			EmploymentDurationType employmentDuration) {
-		super();
-		this.customer = customer;
-		this.age = age;
-		this.affordabilityConcern = affordabilityConcern;
-		this.coverAmount = coverAmount;
-		this.employmentDuration = employmentDuration;
-	}
+   public CustomerProfile()
+   {
 
-	public Customer getCustomer() {
-		return customer;
-	}
+   }
 
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+   /**
+    * @param customer
+    * @param age
+    * @param affordabilityConcern
+    * @param coverAmount
+    * @param employmentDuration
+    */
+   public CustomerProfile(Customer customer, int age,
+         AffordabilityConcernType affordabilityConcern, long coverAmount,
+         EmploymentDurationType employmentDuration)
+   {
+      super();
+      this.customer = customer;
+      this.age = age;
+      this.affordabilityConcern = affordabilityConcern;
+      this.coverAmount = coverAmount;
+      this.employmentDuration = employmentDuration;
+   }
 
-	public int getAge() {
-		return age;
-	}
+   public Customer getCustomer()
+   {
+      return customer;
+   }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+   public void setCustomer(Customer customer)
+   {
+      this.customer = customer;
+   }
 
-	public AffordabilityConcernType getAffordabilityConcern() {
-		return affordabilityConcern;
-	}
+   public int getAge()
+   {
+      return age;
+   }
 
-	public void setAffordabilityConcern(
-			AffordabilityConcernType affordabilityConcern) {
-		this.affordabilityConcern = affordabilityConcern;
-	}
+   public void setAge(int age)
+   {
+      this.age = age;
+   }
 
-	public long getCoverAmount() {
-		return coverAmount;
-	}
+   public AffordabilityConcernType getAffordabilityConcern()
+   {
+      return affordabilityConcern;
+   }
 
-	public void setCoverAmount(long coverAmount) {
-		this.coverAmount = coverAmount;
-	}
+   public void setAffordabilityConcern(
+         AffordabilityConcernType affordabilityConcern)
+   {
+      this.affordabilityConcern = affordabilityConcern;
+   }
 
-	public EmploymentDurationType getEmploymentDuration() {
-		return employmentDuration;
-	}
+   public long getCoverAmount()
+   {
+      return coverAmount;
+   }
 
-	public void setEmploymentDuration(EmploymentDurationType employmentDuration) {
-		this.employmentDuration = employmentDuration;
-	}
+   public void setCoverAmount(long coverAmount)
+   {
+      this.coverAmount = coverAmount;
+   }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "CustomerProfile [customer=" + customer + ", age=" + age
-				+ ", affordabilityConcern=" + affordabilityConcern
-				+ ", coverAmount=" + coverAmount + ", employmentDuration="
-				+ employmentDuration + "]";
-	}
+   public EmploymentDurationType getEmploymentDuration()
+   {
+      return employmentDuration;
+   }
+
+   public void setEmploymentDuration(EmploymentDurationType employmentDuration)
+   {
+      this.employmentDuration = employmentDuration;
+   }
+
+   /* (non-Javadoc)
+    * @see java.lang.Object#toString()
+    */
+   @Override
+   public String toString()
+   {
+      return "CustomerProfile [customer=" + customer + ", age=" + age
+            + ", affordabilityConcern=" + affordabilityConcern
+            + ", coverAmount=" + coverAmount + ", employmentDuration="
+            + employmentDuration + "]";
+   }
+
+   public CustomerProfile(com.kunal.demo.rules.model.Customer customer,
+         int age, long coverAmount)
+   {
+      this.customer = customer;
+      this.age = age;
+      this.coverAmount = coverAmount;
+   }
 
 }
